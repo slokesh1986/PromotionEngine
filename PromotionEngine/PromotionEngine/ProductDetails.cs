772b11e738cc6702eebd4de28e9d53950e319797
slokesh1986 <32222511+slokesh1986@users.noreply.github.com>
2021-05-27 11:54:54 +0000

    Update ProductDetails.cs

--- PromotionEngine/PromotionEngine/ProductDetails.cs
+++ PromotionEngine/PromotionEngine/ProductDetails.cs
@@ -5,5 +5,6 @@
         public char ProductName { get; set; }
         public int Quantity { get; set; }
         public int MyProperty { get; set; }
+        public int Myproperty1 {get;set;}
     }
 }
